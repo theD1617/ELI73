e644fde88ff6e0882267c81e41d9829750cd71db
theD1617 <thedigit@protonmail.com>
2021-02-02 04:54:03 +0100

    fresh

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { v4 as uuidv4 } from "uuid";
-import Order from '../models/order.js';
+import Order from '../models/orders.js';
 
 const router = express.Router();
 
@@ -34,20 +34,6 @@ router.put("/:id",(req, res) => {
                 res.status(404).send("400 dead");
             } else {
                 if(req.body.code) item.code = req.body.code;
-                if(req.body.name) item.name = req.body.name;
-                if(req.body.date) item.date = req.body.date;
-                if(req.body.prod) item.prod = req.body.prod;
-                if(req.body.desc) item.desc = req.body.desc;
-                if(req.body.kat) item.kat = req.body.kat;
-                if(req.body.color) item.color = req.body.color;
-                if(req.body.media) item.media = req.body.media;
-                if(req.body.noten.kopf) item.noten.kopf = req.body.noten.kopf;
-                if(req.body.noten.herz) item.noten.herz = req.body.noten.herz;
-                if(req.body.noten.base) item.noten.base = req.body.noten.base;
-                if(req.body.variants) item.variants = req.body.variants;
-                if(req.body.attributes) item.attributes = req.body.attributes;
-                if(req.body.price) item.price = req.body.price;
-                if(req.body.xdata) item.xdata = req.body.xdata;
             }
             item.save().catch(err => next(err));
         }        
