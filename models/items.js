f56d6d8a4401f3c211db72403fec4f4c817cc2ec
theD1617 <thedigit@protonmail.com>
2021-02-23 10:42:33 +0100

    neptune

--- models/items.js
+++ models/items.js
@@ -3,61 +3,61 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-code:{type: String, required:[true, "required field"]},
-name:{type: String, required:[true, "required field"]},
-date:{type: String},
-prod: {type: String},
-desc:{type: String},
-kat:{type: String},
-color:{type:String},
-gender:{type:String},
-media:[ String ],
-noten:{
-    type:Object,
-    kopf: [ String ],
-    herz: [ String ],
-    base: [ String ],
-    duftnoten: [ String ]   
-},
-variants:[{
-    group: String,
-    variant: String,
-    xcosts: Number,
-    data: String
-}],
-attributes:[{
-    group: String,
-    variant: String,
-    xcosts: Number,
-    data: String,
-    dimensions: {
-        height: Number, 
-        width: Number,
-        depth: Number,
-        gewicht: Number
+    code: { type: String, required: [true, "required field"] },
+    name: { type: String, required: [true, "required field"] },
+    date: { type: String },
+    prod: { type: String },
+    desc: { type: String },
+    kat: { type: String },
+    color: { type: String },
+    gender: { type: String },
+    media: [String],
+    noten: {
+        type: Object,
+        kopf: [String],
+        herz: [String],
+        base: [String],
+        duftnoten: [String]
+    },
+    variants: [{
+        group: String,
+        variant: String,
+        xcosts: Number,
+        data: String
+    }],
+    attributes: [{
+        group: String,
+        variant: String,
+        xcosts: Number,
+        data: String,
+        dimensions: {
+            height: Number,
+            width: Number,
+            depth: Number,
+            gewicht: Number
+        }
+    }],
+    price: {
+        type: Number,
+        required: false
+    },
+    xdata: {
+        type: Object,
+        together: [String],
+        combo: [String],
+        similar: [String],
+        liked: [String],
+        faved: [String],
+        commented: [{
+            user: String,
+            date: Date,
+            msg: String
+        }]
     }
-}],
-price: {
-    type: Number,
-    required: false
-},
-xdata:{
-    type: Object,
-    together: [ String ],
-    combo: [ String ],
-    similar: [ String ],
-    liked: [ String ],
-    faved: [ String ],
-    commented: [{
-        user: String,
-        date: Date,
-        msg: String
-    }]
-}
 
 
 });
 
-const Item = mongoose.model('item',ItemSchema);
+const Item = mongoose.model('item', ItemSchema);
 
 export default Item;
--- models/orders.js
+++ models/orders.js
@@ -5,10 +5,67 @@ const Schema = mongoose.Schema;
 const OrderSchema = new Schema({
     name: {
         type: String,
-        required:[true,"required field"]
+        required: [true, "required field"]
+    },
+    odate: {
+        type: Date,
+        required: false
+    },
+    salesPoint: {
+        type: String,
+        required: true
+    },
+    seller: {
+        type: String,
+        required: true
+    },
+    buyer: {
+        type: String,
+        required: true
+    },
+    order: [
+        {
+            item: {
+                code: {
+                    type: String,
+                    required: true
+                },
+                amount: {
+                    type: Number,
+                    required: true
+                },
+                sprice: {
+                    type: Number,
+                    required: true,
+                },
+                variants: {
+                    type: String,
+                    required: true
+                },
+                attributes: {
+                    type: String,
+                    required: true
+                }
+            }
+        }
+    ],
+    costs: {
+        total: {
+            type: Number,
+            required: true
+        },
+        bonus: {
+            type: Number,
+            required: true
+        },
+        tax: {
+            type: Number,
+            required: true
+        }
     }
+
 });
 
-const Order = mongoose.model('order',OrderSchema);
+const Order = mongoose.model('order', OrderSchema);
 
 export default Order;
